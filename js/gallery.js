1d21b812b07d39bc9ba2b45593c32cde631dc4ae
Ashley <ashleyfleck@gmail.com>
2020-03-18 21:38:57 -0400

    first commit

---
+++ js/gallery.js
@@ -0,0 +1,5 @@
+
+    function showpic(pic) {
+        document.getElementById("img").style.backgroundImage = "url('" + pic.src + "')";
+        document.getElementById("img").innerHTML = "";
+    }
